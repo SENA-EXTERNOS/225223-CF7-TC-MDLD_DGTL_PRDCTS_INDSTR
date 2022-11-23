0eac4ab680d4a7df721901b74ce10d800b8f82ff
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-11-22 21:53:21 -0500

    Avances
    Se termina tema 1 y se avanza con el tema 2

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -6,7 +6,77 @@
       .titulo-principal__numero
         span 1
       h1 Introducción a la manufactura aditiva
+
+    .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5.mb-5
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/temas/T1/img-1.png')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 La fabricación o las tecnologías para de la realización de manufactura aditiva existen en la industria desde 1980 aproximadamente; desde ese entonces siempre ha tenido éxito tanto en el sector de la industria como en el textil, de joyería, medicina, aeronáutica hasta llegar al arquitectónico. Gracias a este tipo de fabricación, se ve con mayor velocidad nuevos objetos en el mercado; en especial en el área industrial.
+
+    p.mb-5 Este tipo de fabricación de un objeto tridimensional con tecnología de manufactura aditiva tiene ciertas consideraciones para tener en cuenta, como lo pueden ser:
       
+  
+    SlyderF.mb-5(columnas="col-lg-6 col-xl-4")
+      .tarjeta.color-slyder-f.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/T1/img-2.svg' alt='AvatarTop')
+        p.text-center Parte de un modelo virtual.
+
+      .tarjeta.color-slyder-f.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/T1/img-3.svg' alt='AvatarTop')
+        p.text-center Se usa un proceso aditivo. 
+
+      .tarjeta.color-slyder-f.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/T1/img-4.svg' alt='AvatarTop')
+        p.text-center En el proceso aditivo el material se va pegando (consolidando) en capas de manera sucesiva hasta tener la pieza requerida.
+
+      .tarjeta.color-slyder-f.p-4
+        .row.justify-content-center.mb-3
+          .col-8
+            img(src='@/assets/curso/temas/T1/img-5.svg' alt='AvatarTop')
+        p.text-center La diferencia de los métodos comunes de fabricación como el corte, el fresado o el mecanizado es que en los anteriores procesos se elimina el material; en vez de añadirlo como se hace en el proceso aditivo.
+
+    p.mb-5 Las ventajas de este tipo de fabricación son:
+
+    .row.justify-content-center.mb-3
+      .col-8
+        TabsA.color-acento-contenido.mb-5      
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Producción directa")
+            h4 Producción directa
+            p Ejecutar la producción de manera directa, reduciendo los tiempos y costos.
+            
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Herramientas especializadas")
+            h4 Herramientas especializadas
+            p La no utilización de herramientas especializadas.
+                
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Reducción de pesos")
+            h4 Reducción de pesos
+            p Reducción considerable de los pesos de los objetos o piezas fabricados, creando así diseños livianos.
+            
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Reducción de unión")
+            h4 Reducción de unión
+            p Se reducen puntos de unión en la piezas u objetos fabricados, debido a que se fabrica por medio de menor cantidad de piezas o incluso solo se hace una pieza.
+            
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Complejidad en el diseño")
+            h4 Complejidad en el diseño
+            p Producir cualquier pieza u objeto que ha sido diseñada de manera virtual o digital con ello se tiene la libertad para el desarrollo de su diseño; con lo anterior se aumenta la complejidad del diseño de la pieza sin tener sobre costo adicionales. 
+
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Pasos cortos")
+            h4 Pasos cortos
+            p En la realización de piezas u objetos complejos estos son producidos en un solo paso, eliminando pasos extensos para su realización.
+
+      .col-3
+        figure
+          img(src="@/assets/curso/temas/T1/img-6.png", alt="Texto que describa la imagen")
+    
+
+
 </template>
 
 <script>
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,7 +5,106 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Primer acercamiento a la impresión
+
+    .row.justify-content-center
+      .col-md-6
+        figure
+          img(src='@/assets/curso/temas/T2/img-1.png', alt='Texto que describa la imagen')
+      .col-md-6.mb-4.mb-md-0
+        p.mb-3 En términos generales la impresión tipo 3D es un proceso de adición progresiva y ordenada de material hasta conformar la pieza que se está consolidación o imprimiendo. Toda esta tecnología ha pasado por varios años de maduración o de desarrollo para consolidarse, haciendo parte de la cuarta revolución industrial propiamente dicha en este momento actual.
+        p.mb-5 Desde los inicios entre los años 1980 y 2000 en donde además de su aparición se evidencia el crecimiento e importancia de la misma por sus múltiples beneficios como la creación de objetos o de piezas, según la necesidad del cliente o del contexto(mercado) toda esta tecnología está muy desarrollada ahora en el área industrial; el punto transversal del presente documento. 
+
+    p.mb-5 La impresión 3D o tridimensional tiene ciertos conceptos o términos que se deben conocer para así poder manejarla con propiedad cuando se realice la impresión. Estos conceptos están relacionados al proceso de la impresión en sí mismo.
+      
+
+    .row.justify-content-center.mb-3
+      .col-5
+        figure
+          img(src="@/assets/curso/temas/T2/img-2.png", alt="Texto que describa la imagen")
+      .col-7
+        AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
+          .row(titulo="Primer Concepto")
+            p El primer concepto es la creación del contenido, es decir la pieza que se diseña; gracias a un software tipo CAD (diseño asistido por computador).
+
+          .row(titulo="Segundo Concepto")
+            p El segundo término o concepto es generar un formato compatible para que así la impresora pueda realizar la impresión. Este formato es denominado formato STL que significa <i>Stereo Lithography</i>.
+
+          .row(titulo="Tercer Concepto")
+            p El siguiente concepto es el rebanado, que es el proceso de traducir el archivo 3D en instrucciones para que la impresora 3D las siga.
+
+          .row(titulo="Cuarto Concepto")
+            p El cuarto concepto es por su puesto el correspondiente a la impresión propiamente dicha.
+
+          .row(titulo="Quinto Concepto")
+            p Después sigue la extracción de la pieza; es decir el retiro del objeto que ya se imprimió, para ello se requieren habilidades profesionales debido a que es un proceso técnico.
+
+          .row(titulo="Sexto Concepto")
+            p El último concepto corresponde al pos- procesamiento de la pieza, en donde se mejoran de manera considerable detalles como los contornos resultantes de la pieza por medio de técnicas adecuadas como el lijado, el pulido, la pintura y otros métodos que se describirán en los siguientes temas.
+          
+    #t_2_1.titulo-segundo.color-acento-botones
+      h2 2.1 Historia y ventajas de la impresión 3D
+
+    p.mb-5 La impresión 3D ha tenido gran desarrollo a través del tiempo; a continuación, se mencionan algunos momentos que han servido de impulso en este desarrollo.
+    
+    .tarjeta.color-linea-tiempo-c.p-4.mb-5
+      LineaTiempoC.color-acento-botones(text-small)
+        .row.justify-content-around.align-items-center(titulo="Inicio")
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/T2/img-3.png', alt='Texto que describa la imagen')
+
+          .col-md-6.mb-4.mb-md-0
+            p Antes de que existiera el proceso de impresión tridimensional o (3D) las piezas o los prototipos se tallaban en madera o se pegaban con trozos de plástico o cartón; lo que implicaba invertir mucho tiempo y además podía tardar varios días o incluso semanas en hacerse; por ende, era común que su precio fuera muy costoso.
+        
+        .row.justify-content-around.align-items-center(titulo="Funciones <br>básicas")
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/T2/img-4.png', alt='Texto que describa la imagen')
+
+          .col-md-6.mb-4.mb-md-0
+            p El autor de ciencia ficción Arthur Clarke fue uno de los primeros en describir las diferentes funciones a nivel básico de una impresora tipo 3D en el año 1964.
+
+        .row.justify-content-around.align-items-center(titulo="Primera <br>impresora 3D")
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/T2/img-5.png', alt='Texto que describa la imagen')
+
+          .col-md-6.mb-4.mb-md-0
+            p La historia cuenta que la primera impresora 3D fue lanzada en el año 1987 gracias a la empresa 3D <i>Systems</i> y para ese tiempo se utilizaba el proceso de «estereolitografía» (SLA) que perdura en la actualidad.
+
+        .row.justify-content-around.align-items-center(titulo="Otras <br>tecnologías")
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/T2/img-6.png', alt='Texto que describa la imagen')
+
+          .col-md-6.mb-4.mb-md-0
+            p En lo que respecta a los años 1900 y 2000 se lanzaron otras impresoras 3D con distintas tecnologías, en la empresa <i>Stratasys</i> y de la empresa 3D <i>System</i>; en esa época estas máquinas tenían un costo alto y su utilización era principalmente en el área de la creación de prototipos de tipo industrial.
+
+        .row.justify-content-around.align-items-center(titulo="Fabricación <br>aditiva")
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/T2/img-7.png', alt='Texto que describa la imagen')
+
+          .col-md-6.mb-4.mb-md-0
+            p En el año 2009 se publicó un documento con la terminología general sobre fabricación aditiva y en este se estableció la impresión 3D como una tecnología de fabricación de tipo industrial.
+
+        .row.justify-content-around.align-items-center(titulo="Impresoras de <br>mesa")
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/T2/img-8.png', alt='Texto que describa la imagen')
+
+          .col-md-6.mb-4.mb-md-0
+            p Después del año 2009 nacieron las primeras impresoras 3D de mesa que eran de bajo costo.
+
+        .row.justify-content-around.align-items-center(titulo="Crecimiento")
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/T2/img-9.png', alt='Texto que describa la imagen')
+
+          .col-md-6.mb-4.mb-md-0
+            p En los años subsiguientes, la adopción de la impresión 3D sigue creciendo y entre 2015 y 2017 ya que se vendieron más de un millón de impresoras 3D de mesa en todo el mundo y las ventas de impresoras industriales tipo (metal) se duplicaron en 2017 respecto al año inmediatamente anterior.
+          
 
 </template>
 
