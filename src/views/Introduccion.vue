c860808ab38d61dcfc2807c776018894631479ed
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-01-30 12:21:21 -0500

    Se vinculan los dos videos.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -21,8 +21,8 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/ScZYcQuZh-4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video: Tecnologías para la manufactura aditiva y tipos de impresión 3D
 
 
 
